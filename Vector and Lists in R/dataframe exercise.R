face24ad61e75d9fa30a3af2110c2a16db951e49
Sulekha mohamud <sulekhasabah@gmail.com>
2022-12-10 22:32:58 +0100

    Add files via upload

---
+++ Vector and Lists in R/dataframe exercise.R
@@ -0,0 +1,8 @@
+#creating dataframs
+#assigning all the values from the right to d
+d<-data.frame(col1=c(1,2,3), col2=c(-1,0,1))
+#col3 is the sum of col2 and col1
+d$col3<-d$col1+d$col2
+print(d)
+
+################end of exercise###################
